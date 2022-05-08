78afdf2e69da7b28d734444ab016e1d968831e72
s1dewalker <92365755+s1dewalker@users.noreply.github.com>
2022-05-08 23:48:11 +0530

    Update tracker_app.py

--- tracker_app.py
+++ tracker_app.py
@@ -484,18 +484,6 @@ with colc:
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
                                                                #ADDING LINKS
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
-gif_html = get_img_with_href('pics/github-32.png', 'https://github.com/s1dewalker/NSE_Tracker_App')
-st.markdown(gif_html, unsafe_allow_html=True)
-st.markdown("")
-gif_html = get_img_with_href('pics/linkedin-32.png', 'http://www.linkedin.com/in/sujay-bhaumik-d12')
-st.markdown(gif_html, unsafe_allow_html=True)
-st.markdown("")
-gif_html = get_img_with_href('pics/gmail-32.png', 'mailto:sujaybhk99@gmail.com')
-st.markdown(gif_html, unsafe_allow_html=True)
-st.markdown("")
-gif_html = get_img_with_href('pics/tableau-32.png', 'https://public.tableau.com/app/profile/sujay.bahumik#!/?newProfile=&activeTab=0')
-st.markdown(gif_html, unsafe_allow_html=True)
-
 gif_html1 = get_img_with_href('pics/github-32.png', 'https://github.com/s1dewalker/NSE_Tracker_App')
 gif_html2 = get_img_with_href('pics/linkedin-32.png', 'http://www.linkedin.com/in/sujay-bhaumik-d12')
 gif_html3 = get_img_with_href('pics/gmail-32.png', 'mailto:sujaybhk99@gmail.com')
